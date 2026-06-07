7647a5b2fd9aa602b0c2f45b5d981759c47396ba
ash <291588976+H-rf@users.noreply.github.com>
2026-06-07 23:53:07 +0100

    Add balance API route

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from ledger import BudgetLedger
 
 app = FastAPI()
+
+ledger = BudgetLedger()
 
 
 @app.get("/")
 def home():
     return {"message": "Budget Ledger API"}
+
+
+@app.get("/balance")
+def get_balance():
+    return ledger.get_balance()
