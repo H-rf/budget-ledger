1b8dec9e0d97548f186f455f2b87afd0bf34cd78
ash <291588976+H-rf@users.noreply.github.com>
2026-06-09 16:18:43 +0100

    Add first API test

---
+++ test_api.py
@@ -0,0 +1,15 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_balance_initially_zero():
+    response = client.get("/balance")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "income": 0,
+        "expense": 0,
+        "balance": 0
+    }
